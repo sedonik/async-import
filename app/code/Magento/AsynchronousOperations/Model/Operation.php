cc268d7558c96fdfebe1e963c8f779043d7d23c4
Sergey Dovbenko <sdovbenko@magecom.us>
2019-08-29 22:05:13 +0000

    Updated Code Styles

--- app/code/Magento/AsynchronousOperations/Model/Operation.php
+++ app/code/Magento/AsynchronousOperations/Model/Operation.php
@@ -22,12 +22,12 @@ class Operation extends DataObject implements OperationInterface
     /**
      * Operation constructor.
      *
-     * @param array $data
      * @param OperationStatusValidator $operationStatusValidator
+     * @param array $data
      */
     public function __construct(
-        array $data = [],
-        OperationStatusValidator $operationStatusValidator
+        OperationStatusValidator $operationStatusValidator,
+        array $data = []
     ) {
         $this->operationStatusValidator = $operationStatusValidator;
         parent::__construct($data);
